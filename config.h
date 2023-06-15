3a403363d173dce656e2f7a3b211afafb71c3acd
Luca Bilke <bilke@tralios.de>
2023-06-15 09:52:06 +0200

    allow scrolling with mousewheel

--- config.h
+++ config.h
@@ -200,10 +200,8 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 3}         }, /* You can increase .i to increase scroll speed*/
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 3}         },
 };
 
 /* Internal keyboard shortcuts. */
